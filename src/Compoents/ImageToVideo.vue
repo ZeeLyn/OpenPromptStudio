60d6fb72b6393b42303637b73b95f71e40510391
Owen <zeelyn@qq.com>
2024-04-27 22:10:23 +0800

    dev

--- src/Compoents/ImageToVideo.vue
+++ src/Compoents/ImageToVideo.vue
@@ -20,7 +20,7 @@
                     @drop.prevent="DropImageFileHandle"
                     @click="clickHandle"
                 >
-                    <img class="preview" :src="imageUrl" v-if="imageUrl" />
+                    <img class="preview" :src="config.imageUrl" v-if="config.imageUrl" />
                     <template v-else> 请点击上传或拖入图片文件 </template>
                 </div>
                 <input type="file" ref="file" accept=".jpg,.jpeg,.png" @change="FileChangeHandle" />
@@ -28,7 +28,8 @@
             <el-form-item label="提示词" prop="prompt">
                 <el-input type="textarea" v-model="config.prompt"></el-input>
                 <el-alert
-                    title="消息提示的文案"
+                    v-if="config.translate_prompt"
+                    :title="config.translate_prompt"
                     type="info"
                     effect="dark"
                     :closable="false"
@@ -47,7 +48,42 @@
             <el-form-item label="FPS" prop="fps">
                 <el-slider v-model="config.fps" :min="1" :max="30" :format-tooltip="formatDurationTooltip"></el-slider>
             </el-form-item>
+            <el-form-item> </el-form-item>
+            <el-button
+                type="primary"
+                @click="submitForm"
+                :loading="loading"
+                style="width: 100%"
+                :disabled="!config.imageUrl || !config.prompt || !config.translate_prompt || translateing"
+                >生成视频</el-button
+            >
         </el-form>
+        <el-dialog
+            title="视频预览"
+            :visible.sync="dialogVisible"
+            width="50%"
+            :close-on-click-modal="false"
+            :destroy-on-close="true"
+        >
+            <video
+                muted
+                autoplay
+                :src="'https://web.inlarks.com/aigc_prompt/output/image_to_video/' + taskId + '.mp4'"
+                controls
+                style="width: 100%"
+                loop
+            ></video>
+            <span slot="footer" class="dialog-footer">
+                <el-link
+                    type="primary"
+                    icon="el-icon-download"
+                    :href="'https://web.inlarks.com/aigc_prompt/output/image_to_video/' + taskId + '.mp4'"
+                    :download="taskId + '.mp4'"
+                    target="_blank"
+                    >下载视频</el-link
+                >
+            </span>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -58,145 +94,118 @@ export default {
     data() {
         return {
             dragEnter: false,
-            imageUrl: "",
+            loading: false,
             config: {
-                duration: 3,
-                fps: 10,
+                imageUrl: "",
+                prompt: "",
+                duration: 2,
+                fps: 8,
+                translate_prompt: "",
             },
+            dialogVisible: false,
+            translateing: false,
             formRules: {
-                source: [
+                imageUrl: [
                     {
                         required: true,
-                        message: "请选择需要识别的图片目录",
+                        message: "请上传图片",
                         trigger: "blur",
                     },
                 ],
-                options: [
+                prompt: [
                     {
                         required: true,
-                        message: "请设置投票有多少个选项",
+                        message: "请设置提示词",
                         trigger: "blur",
-                    },
-                ],
-                optionList: [
-                    {
-                        required: true,
-                        message: "请设置投票选项",
-                        trigger: "blur",
-                    },
-                    {
-                        validator: function (rule, value, callback) {
-                            if (Object.keys(this.config.optionList).length < this.config.options) {
-                                callback(`请填写选项`)
-                                return
-                            }
-                            for (var i = 0; i < this.config.options; i++) {
-                                if (!this.config.optionList[`option_${i + 1}`]) {
-                                    callback(`第${i + 1}个选项未填写`)
-                                } else {
-                                    callback()
-                                }
-                            }
-                        }.bind(this),
-                    },
-                ],
-                pages: [
-                    {
-                        required: true,
-                        message: "请设置一份完整的投票有几页",
-                        trigger: "blur",
-                    },
-                ],
-                pageList: [
-                    {
-                        required: true,
-                        message: "请设置一份完整的投票有几页",
-                        trigger: "blur",
-                    },
-                    {
-                        validator: function (rule, value, callback) {
-                            if (Object.keys(this.config.pageList).length < this.config.pages) {
-                                callback(`请设置每页表格投票的行和列`)
-                                return
-                            }
-                            if (this.config.direction == 1) {
-                                var listCount = 0
-                                for (var i = 0; i < this.config.pages; i++) {
-                                    if (!this.config.pageList[`page_${i + 1}`]) {
-                                        callback(`第${i + 1}页未设置`)
-                                        return
-                                    }
-
-                                    var page = this.config.pageList[`page_${i + 1}`]
-                                    if (page.endCol - page.startCol + 1 != this.config.options) {
-                                        callback(`第${i + 1}页的开始列和结束列跟投票选项数量不一致`)
-                                        return
-                                    }
-                                    listCount += page.endRow - page.startRow + 1
-                                }
-                                if (listCount != this.config.list.length) {
-                                    callback(`${this.config.pages}页的总行数跟投票名单数量不一致`)
-                                    return
-                                }
-                                callback()
-                            } else {
-                                var listCount = 0
-                                for (var i = 0; i < this.config.pages; i++) {
-                                    if (!this.config.pageList[`page_${i + 1}`]) {
-                                        callback(`第${i + 1}页未设置`)
-                                        return
-                                    }
-
-                                    var page = this.config.pageList[`page_${i + 1}`]
-                                    if (page.endCol - page.startCol + 1 != this.config.list.length) {
-                                        callback(`第${i + 1}页的开始列和结束列跟投票名单数量不一致`)
-                                        return
-                                    }
-                                    listCount += page.endRow - page.startRow + 1
-                                }
-                                if (listCount != this.config.options) {
-                                    callback(`${this.config.pages}页的总行数跟投票选项数量不一致`)
-                                    return
-                                }
-                                callback()
-                            }
-                        }.bind(this),
-                    },
-                ],
-                list: [
-                    {
-                        required: true,
-                        message: "请上传投票名单",
-                        trigger: "blur",
-                    },
-                    {
-                        validator: function (rule, value, callback) {
-                            if (!this.config.list || this.config.list.length == 0) {
-                                callback(`请上传投票名单`)
-                            } else {
-                                callback()
-                            }
-                        }.bind(this),
                     },
                 ],
             },
+            taskId: "",
         }
     },
     watch: {
         "config.prompt": function (v) {
-            console.log(v)
+            this.last_input_time = new Date().getTime()
         },
     },
-    mounted() {},
+    mounted() {
+        this.last_input_time = new Date().getTime()
+        var self = this
+        var prev_translate_prompt = ""
+        this.translateTimer = setInterval(() => {
+            if (new Date().getTime() - self.last_input_time > 500) {
+                if (self.config.prompt == prev_translate_prompt || !self.config.prompt) return
+
+                prev_translate_prompt = self.config.prompt
+                if (!self.hasChinese(self.config.prompt)) {
+                    self.config.translate_prompt = self.config.prompt
+                    return
+                }
+                self.translateing = true
+                axios
+                    .post("https://app.api.inlarks.com/tencent_translate/api/translate/request", {
+                        sourceText: self.config.prompt,
+                    })
+                    .then((res) => {
+                        self.config.translate_prompt = res.data.targetText
+                    })
+                    .catch((err) => {
+                        self.config.translate_prompt = self.config.prompt
+                    })
+                    .finally(() => {
+                        self.translateing = false
+                    })
+            }
+        }, 500)
+    },
     methods: {
+        hasChinese(str) {
+            var reg = /[\u4e00-\u9fa5]/
+            return reg.test(str)
+        },
+        submitForm() {
+            this.$refs.form.validate((valid) => {
+                if (valid) {
+                    this.loading = true
+                    let formData = new FormData()
+                    formData.append("file", this.image)
+                    formData.append("prompt", this.config.translate_prompt)
+                    formData.append("duration", this.config.duration)
+                    formData.append("fps", this.config.fps)
+                    axios
+                        .post("http://192.168.3.55:5000/api/data", formData)
+                        .then((res) => {
+                            this.taskId = res.data.id
+                            this.checStatus()
+                        })
+                        .catch((err) => {
+                            this.loading = false
+                        })
+                }
+            })
+        },
+        checStatus() {
+            var self = this
+            this.timer = setInterval(function () {
+                axios
+                    .get(`http://192.168.3.55:5000/api/status/${self.taskId}`)
+                    .then((res) => {
+                        if (res.data.status == 1) {
+                            self.video = res.data.message
+                            self.loading = false
+                            self.dialogVisible = true
+                            clearInterval(self.timer)
+                        }
+                    })
+                    .then()
+            }, 3000)
+        },
         formatDurationTooltip(v) {
             return `${v}秒`
         },
         DropImageFileHandle(e) {
             this.dragEnter = false
-            console.log(e.dataTransfer.files.length)
             const file = e.dataTransfer.files[0]
-            console.log(file)
             if (!["jpg", "jpeg", "png"].includes(this.getFileExtension(file.name).toLowerCase())) {
                 this.$message({ message: "只支持jpg,png格式的图片", type: "error" })
                 return
@@ -217,25 +226,9 @@ export default {
             this.image = file
             var reader = new FileReader()
             reader.onload = function (e) {
-                self.imageUrl = e.target.result
+                self.config.imageUrl = e.target.result
             }
             reader.readAsDataURL(file)
-            let formData = new FormData()
-            formData.append("file", file)
-            formData.append("prompt", "abc")
-            axios
-                .post("http://192.168.3.55:5000/api/data", formData)
-                .then((res) => {
-                    console.log(res)
-                    this.id = res.data.id
-                    clearInterval(this.timer)
-                    this.checkStatus()
-                })
-                .catch((err) => {
-                    console.log(err)
-                    this.loading = false
-                    this.img = null
-                })
         },
         ImportList(file) {
             var self = this
@@ -264,6 +257,9 @@ export default {
             return extension
         },
     },
+    beforeDestroy() {
+        clearInterval(this.translateTimer)
+    },
 }
 </script>
 <style scoped>
@@ -277,6 +273,9 @@ export default {
     font-size: 30px;
     color: #ccc;
     cursor: pointer;
+}
+.drag-wrap:hover {
+    border: 1px #666 solid;
 }
 .draging {
     background: #0eb964;
